3efe07e82a2dd161cbd55fc478b9ee8a365ca7f1
arthurcicero <arthur-cicero@hotmail.com>
2019-03-04 22:08:14 -0300

    rot13 implementation

--- rot13.py
+++ rot13.py
@@ -1,1 +1,15 @@
-test
+import string
+
+word = input("word: ").lower()
+
+lowercase = string.ascii_lowercase
+
+print("rot13: ", end='')
+for i in range(len(word)):
+    for j in range(len(lowercase)):
+        if word[i] == lowercase[j]:
+            if j >= 13: print(lowercase[j-13], end='') #first 13 letters
+            else: print(lowercase[j+13], end='') #last 13 letters
+            break
+
+print(" ")
