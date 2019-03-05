eb35d33f53892766919151849e5f9266449d6a36
arthurcicero <arthur-cicero@hotmail.com>
2019-03-04 22:11:31 -0300

    comments added

--- rot13.py
+++ rot13.py
@@ -2,14 +2,14 @@ import string
 
 word = input("word: ").lower()
 
-lowercase = string.ascii_lowercase
+lowercase = string.ascii_lowercase #abcdefghijklmnopqrstuvwxyz
 
 print("rot13: ", end='')
 for i in range(len(word)):
     for j in range(len(lowercase)):
-        if word[i] == lowercase[j]:
-            if j >= 13: print(lowercase[j-13], end='') #first 13 letters
-            else: print(lowercase[j+13], end='') #last 13 letters
+        if word[i] == lowercase[j]: #search for letter
+            if j >= 13: print(lowercase[j-13], end='') #for the first 13 letters
+            else: print(lowercase[j+13], end='') #for the last 13 letters
             break
 
 print(" ")
